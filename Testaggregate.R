634e4ab3f7a78aa91b5f045367e68bb04c586bc8
Baldewicz <33531@icf.com>
2017-02-10 09:57:55 -0500

    init

---
+++ Testaggregate.R
@@ -0,0 +1,21 @@
+#Testaggregate
+
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(tidyr)
+
+
+df<- diamonds
+
+#Want table of prices, factor on cut
+
+df2 <- df %>% group_by(cut) %>% summarize(newprice = mean(price))
+
+df3 <- df %>% group_by(cut) %>% summarize(newct = n())
+
+sum(df3$newct)
+
+df4 <- df %>% group_by(cut,clarity) %>% summarize(newct = n())
+
+df5 <- spread(df4,cut,newct)
