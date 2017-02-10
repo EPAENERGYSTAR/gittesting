d2c159ea4a4957cb40f65a76cd525493f39bf719
DanBaldewicz <Dan.Baldewicz@gmail.com>
2017-02-10 11:13:24 -0500

    Added Some Tableplots

--- Testaggregate.R
+++ Testaggregate.R
@@ -4,9 +4,11 @@ library(ggplot2)
 library(data.table)
 library(dplyr)
 library(tidyr)
-
+library(tabplot)
 
 df<- diamonds
+
+tableplot(df)
 
 #Want table of prices, factor on cut
 
@@ -15,7 +17,13 @@ df2 <- df %>% group_by(cut) %>% summarize(newprice = mean(price))
 df3 <- df %>% group_by(cut) %>% summarize(newct = n())
 
 sum(df3$newct)
+tableplot(df3)
+
 
 df4 <- df %>% group_by(cut,clarity) %>% summarize(newct = n())
 
+tableplot(df4)
+
 df5 <- spread(df4,cut,newct) 
+
+tableplot(df5)
